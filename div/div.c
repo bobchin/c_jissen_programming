c1fd1ac73826361de7a25608dc86b98090cb3473
bobchin <bobchin.ryu@gmail.com>
2015-09-11 11:39:13 +0900

    例4-6

---
+++ div/div.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+float result;
+
+int main()
+{
+  result = 7.0 / 22.0;
+  printf("The result is %d\n", result);
+  /* printf("The result is %f\n", result); */
+  return (0);
+}
