a905b4ea05884464f2345c7db330c100fe3a288b
bobchin <bobchin.ryu@gmail.com>
2015-09-11 15:51:44 +0900

    実習6-4を修正

--- owe0/owe0.c
+++ owe0/owe0.c
@@ -8,7 +8,7 @@ int main()
   printf("Enter number of dollars owed:");
   fgets(line, sizeof(line), stdin);
   sscanf(line, "%d", &balance_owed);
-  if (balance_owed = 0)
+  if (balance_owed == 0)
     printf("You owe nothing.\n");
   else
     printf("You owe %d dollars.\n", balance_owed);
