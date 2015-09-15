1de4d1673a8c1c4286c84d6b8745f663f928008c
bobchin <bobchin.ryu@gmail.com>
2015-09-15 17:20:21 +0900

    例13-13

---
+++ tmp-name/tmp2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * tmp_name -- 一時的なファイル名を返す
+ *
+ * この関数は呼び出されるたびに、新しい名前を返す
+ *
+ * 戻り値
+ *    新しいファイル名を指すポインタ
+ */
+char *tmp_name(void)
+{
+  static char name[30];
+  static int sequence = 0;
+
+  ++sequence;
+
+  strcpy(name, "tmp");
+  name[3] = sequence + '0';
+  name[4] = '\0';
+
+  return name;
+}
+
+int main()
+{
+  char *tmp_name(void);
+
+  printf("Name: %s\n", tmp_name());
+  return (0);
+}
