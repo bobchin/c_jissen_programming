419feda59f539f1fcea500110dcd153d9cd55c35
bobchin <bobchin.ryu@gmail.com>
2015-09-16 10:00:26 +0900

    例14-4

---
+++ wbin/wbin.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef __MSDOS__
+#include <unistd.h>
+#endif
+
+int main()
+{
+  int cur_char;
+  FILE *out_file;
+
+  out_file = fopen("test.out", "w");
+  if (out_file == NULL) {
+    fprintf(stderr, "Cannot open output file\n");
+    exit(8);
+  }
+
+  for (cur_char = 0; cur_char < 128; cur_char++) {
+    fputc(cur_char, out_file);
+  }
+  fclose(out_file);
+  return (0);
+}
