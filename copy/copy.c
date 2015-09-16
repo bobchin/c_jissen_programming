316a43c5ab7e3984f9c4b44ac4c18bec22c9d7b7
bobchin <bobchin.ryu@gmail.com>
2015-09-16 09:37:38 +0900

    例14-1

---
+++ copy/copy.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+const char FILE_NAME[] = "input.txt";
+#include <stdlib.h>
+
+int main()
+{
+  int count = 0;
+  FILE *in_file; /* 入力ファイル */
+
+  int ch;
+
+  in_file = fopen(FILE_NAME, "r");
+  if (in_file == NULL) {
+    printf("Cannot open %s\n", FILE_NAME);
+    exit(8);
+  }
+
+  while (1) {
+    ch = fgetc(in_file);
+    if (ch == EOF)
+      break;
+    ++count;
+  }
+
+  printf("Number of charactors in %s is %d\n", FILE_NAME, count);
+
+  fclose(in_file);
+  return (0);
+}
