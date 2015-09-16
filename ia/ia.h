0a2b6ee9214da775c6174cb014c07bdb73b80963
bobchin <bobchin.ryu@gmail.com>
2015-09-16 13:49:06 +0900

    例18-1

---
+++ ia/ia.h
@@ -0,0 +1,58 @@
+/*
+ * 無限配列(ia)パッケージに関する定義
+ *
+ * 無限配列は必要に応じてサイズが大きくなる配列である。
+ * 配列に最初の指定以上の要素を追加すると配列は大きくなる。
+ * --------------------------------------------
+ * 構造体 infinity_array
+ *       無限配列に関する情報を格納するために使用
+ * --------------------------------------------
+ * ルーチン
+ *
+ *     ia_init  -- 配列を初期化
+ *     ia_store -- 配列に要素を格納
+ *     ia_get   -- 配列の要素を取得
+ */
+
+/* 無限配列の各セルに格納する要素の数 */
+#define BLOCK_SIZE 10
+
+struct infinity_array {
+  /* このブロックのデータ */
+  float data[BLOCK_SIZE];
+
+  /* 次の配列へのポインタ */
+  struct infinity_array *next;
+};
+
+/*
+ * ia_init -- 無限配列を初期化
+ *
+ * パラメータ
+ *    array_ptr -- 初期化する配列
+ */
+#define ia_init(array_ptr) {(array_ptr)->next = NULL;}
+
+/*
+ * ia_get -- 配列の要素を取得
+ *
+ * パラメータ
+ *     array_ptr -- 使用する配列へのポインタ
+ *     index -- 配列のインデックス
+ * 戻り値
+ *     要素の値
+ *
+ * 注意：前もって格納していない要素を取得することが可能である。
+ *      未初期化の要素の値はすべて0となる。
+ */
+extern int ia_get(struct infinity_array *array_ptr, int index);
+
+/*
+ * ia_store -- 無限配列に要素を格納
+ *
+ * パラメータ
+ *     array_ptr -- 使用する配列へのポインタ
+ *     index -- 配列のインデックス
+ *     store_data -- 格納するデータ
+ */
+extern void ia_store(struct infinity_array *array_ptr, int index, int store_data);
